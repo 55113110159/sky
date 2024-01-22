bc174c6fde1e9df41e53bf2940eb2a920bae9b8d
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-23 00:17:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, redirect
+import logging
 
 app = Flask(__name__)
 
+# Configure the logging
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('app.log'),  # Log to a file
+        logging.StreamHandler()  # Log to the console
+    ]
+)
+
+# Example route that generates log messages
 @app.route('/')
-def redirect_to_website():
-    # Replace 'https://www.example.com' with the desired website URL
+def index():
+    app.logger.debug('This is a debug message')
+    app.logger.info('This is an info message')
+    app.logger.warning('This is a warning message')
+    app.logger.error('This is an error message')
+    app.logger.critical('This is a critical message')
     return redirect("https://trustedge.com/", code = 302)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
