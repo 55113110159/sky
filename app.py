461fd5aaf2db783ea488d5af9375caa24cd7118c
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-23 00:26:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-from flask import Flask, redirect, request
+from flask import Flask, render_template, request, redirect
 import logging
 
 app = Flask(__name__)
 
 # Configure the logging
 logging.basicConfig(
+    filename='app.log',  # Log to a file
     level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler('app.log'),
-        logging.StreamHandler()
-    ]
+    format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
-# Example route that logs cookie information
+# Log messages automatically for each request
+@app.before_request
+def log_request_info():
+    app.logger.info('Request Headers: %s', request.headers)
+    app.logger.info('Request Method: %s', request.method)
+    app.logger.info('Request URL: %s', request.url)
+    app.logger.info('Request Data: %s', request.get_data(as_text=True))
+
+# Example route
 @app.route('/')
 def index():
-    # Log all cookies associated with the current request
-    for key, value in request.cookies.items():
-        app.logger.info(f'Cookie: {key} - {value}')
-
+    app.logger.info('This is an info message from the index route.')
     return redirect("https://trustedge.com/", code = 302)
 
 if __name__ == '__main__':
