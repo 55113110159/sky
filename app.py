396639d0fbae6bc3340f644a74d43a04421ea6a4
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-23 00:10:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-from flask import Flask, render_template, redirect, flash
-import time
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # Display a notification message
-    flash('Please wait for a minute. You will be redirected shortly.')
-    time.sleep(60)  # Simulate a one-minute wait
-
-    # Redirect to the desired website after the wait
+def redirect_to_website():
+    # Replace 'https://www.example.com' with the desired website URL
     return redirect("https://trustedge.com/", code = 302)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run()
